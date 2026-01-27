3e935c632565f9ef967a55b2a65f47efdec41797
JesusT491 <jtorres217@cps.edu>
2026-01-27 20:28:01 +0000

    yes

--- 1_.py
+++ 1_.py
@@ -1,3 +1,56 @@
+# function = A block of reusable code
+#               place () after the function name to invoke/call it
+
+# def happy_birthday(name,age):
+#     print(f"Happy Birthday to {name}!")
+#     print(f"Your are {age} years old!")
+#     print("Happy birthday to you!")
+#     print()
+
+# happy_birthday("JESUS",67)
+# happy_birthday("steve", 40)
+# happy_birthday("joe bart", 30)
+
+# def display_invoice(username,amount,due_date):
+#     print(f"Hello {username}")
+#     print(f"Your bill of ${amount:.2f} is due: {due_date}")
+
+# display_invoice("Joe Bart", 42.50, "01/01")
+# display_invoice("Joe Bart II", 67, "05/23")
+# display_invoice("Joe Bart III", 65, "06/32")
+
+# return = statements used to end a function and send a result back to the caller
+
+# def add(x,y):
+#     z = x + y
+#     return
+
+# def subtract(x,y):
+#     z = x - y
+#     return z
+
+# def multiply(x,y):
+#     z = x * y
+#     return z
+
+# def divide(x,y):
+#     z = x / y
+#     return z
+
+# print(add(1,2))
+# print(subtract(1,2))
+# print(multiply(1,2))
+# print(divide(1,2))
+
+def create_name(first,last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("Joe", "Bart")
+
+print(full_name)
+
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
 
